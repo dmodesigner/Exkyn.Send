03eb83791a1c332749feb08a767f14dd2ae9d2fb
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2023-11-30 15:13:10 -0300

    Adicionado novas validações e refatoramento para minimizar as possibilidades de erros. Adicionado a possibilidade de colocar um display name para o enviodo e-mail

--- Src/Exkyn.Send/Email/Send.cs
+++ Src/Exkyn.Send/Email/Send.cs
@@ -1,5 +1,4 @@
-﻿using Exkyn.Core.Helpers;
-using System.Net;
+﻿using System.Net;
 using System.Net.Mail;
 
 namespace Exkyn.Send.Email
@@ -9,39 +8,23 @@ namespace Exkyn.Send.Email
         private MailMessage _mailMessage;
         private SmtpClient _smtpClient;
 
-        private string GetSmtpHost(string email) => "smtp." + email.Split('@')[1].Trim();
-
-        private void ValidateEmail(string email)
-        {
-            ValidateString(email, "Informe o e-mail que irá disparar o envio do e-mail.");
-
-            if (!ValidateHelpers.Email(email))
-                throw new ArgumentException("Informe um e-mail valido.");
-        }
-
-        private void ValidateString(string value, string message)
-        {
-            if (string.IsNullOrEmpty(value))
-                throw new ArgumentException(string.IsNullOrEmpty(message) ? "A variável não pode ser vazia ou nula" : message);
-        }
-
         private void AddEmail(string email)
         {
-            ValidateEmail(email);
+            Validate.ValidateEmail(email);
 
             _mailMessage.To.Add(email);
         }
 
         private void AddSubject(string subject)
         {
-            ValidateString(subject, "Informe um assunto");
+            Validate.ValidateString(subject, "Informe um assunto");
 
             _mailMessage.Subject = subject;
         }
 
         private void AddMessage(string message)
         {
-            ValidateString(message, "Informe uma mensagem");
+            Validate.ValidateString(message, "Informe uma mensagem");
 
             _mailMessage.Body = message;
         }
@@ -54,7 +37,12 @@ namespace Exkyn.Send.Email
             }
             catch (Exception e)
             {
-                throw new ArgumentException(e.Message);
+                var msgErro = e.Message;
+
+                if (e.InnerException != null && !string.IsNullOrEmpty(e.InnerException.Message))
+                    msgErro = e.InnerException.Message;
+
+                throw new Exception(msgErro);
             }
         }
 
@@ -63,11 +51,9 @@ namespace Exkyn.Send.Email
             if (sendingEmailConfiguration is null)
                 throw new ArgumentNullException("Informe um objeto de configuração para o envio do e-mail.");
 
-            ValidateEmail(sendingEmailConfiguration.Email);
+            Validate.ValidateString(sendingEmailConfiguration.Host, "Informe um host valido");
 
-            ValidateString(sendingEmailConfiguration.Password, "Informe a senha do e-mail que enviara a mensagem.");
-
-            _smtpClient = new SmtpClient(GetSmtpHost(sendingEmailConfiguration.Email));
+            _smtpClient = new SmtpClient(sendingEmailConfiguration.Host);
 
             _smtpClient.Port = sendingEmailConfiguration.Port;
             _smtpClient.EnableSsl = sendingEmailConfiguration.UseSsl;
@@ -76,7 +62,7 @@ namespace Exkyn.Send.Email
 
             _mailMessage = new MailMessage();
 
-            _mailMessage.From = new MailAddress(sendingEmailConfiguration.Email);
+            _mailMessage.From = new MailAddress(sendingEmailConfiguration.Email, sendingEmailConfiguration.DisplayName);
             _mailMessage.IsBodyHtml = true;
         }
 
--- Src/Exkyn.Send/Email/SendingEmailConfiguration.cs
+++ Src/Exkyn.Send/Email/SendingEmailConfiguration.cs
@@ -2,8 +2,34 @@
 {
     public class SendingEmailConfiguration
     {
-        public string? Email { get; set; }
-        public string? Password { get; set; }
+        private string GetSmtpHost(string email) => "mail." + email.Split('@')[1].Trim();
+
+        public SendingEmailConfiguration(string email, string password)
+        {
+            Validate.ValidateEmail(email);
+            Validate.ValidateString(password, "Informe o password");
+
+            Email = email;
+            Password = password;
+            Host = GetSmtpHost(email);
+        }
+
+        public SendingEmailConfiguration(string displayName, string email, string password)
+        {
+            Validate.ValidateEmail(email);
+            Validate.ValidateString(password, "Informe o password");
+            Validate.ValidateString(displayName, "Informe o display name");
+
+            DisplayName = displayName;
+            Email = email;
+            Password = password;
+            Host = GetSmtpHost(email);
+        }
+
+        public string? DisplayName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
         public ushort Port { get; set; } = 587;
         public bool UseSsl { get; set; }
     }
