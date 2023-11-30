12424e5ae5ecdf71bbcc38d1c69e38cd951ca6d0
Daniel Moura de Oliveira <daniel.oliveira@gtisolution.com.br>
2023-11-30 15:15:27 -0300

    Adicionado o projeto de teste.

---
+++ Src/Exkyn.Send.Test/Email/SendTest.cs
@@ -0,0 +1,36 @@
+﻿using Exkyn.Send.Email;
+
+namespace Exkyn.Send.Test.Email
+{
+    [TestClass]
+    public class SendTest
+    {
+        private readonly SendingEmailConfiguration _config;
+        private const string _subject = "Mensagem de Teste";
+        private const string _message = "Mensagem de teste, disparado pelo sistema de teste.";
+
+        public SendTest()
+        {
+            _config = new SendingEmailConfiguration("seu_email@email.com.br", "senha_do_email");
+        }
+
+        [TestMethod]
+        public void EnviaParaUmEmail()
+        {
+            var sendEmail = new Send.Email.Send(_config);
+            sendEmail.SendEmail("email@email.com.br", _subject, _message);
+        }
+
+        [TestMethod]
+        public void EnviaParaUmaListaDeEmail()
+        {
+            var sendEmail = new Send.Email.Send(_config);
+            
+            sendEmail.SendEmail(new List<string>
+            {
+                "email1@email.com.br",
+                "email2@email.com.br"
+            }, _subject, _message);
+        }
+    }
+}
